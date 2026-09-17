6e7721e8c201b903cb924fa069e8fae489cfe4d2
Baryali Behrooz <baryalaibehroz2@gmail.com>
2026-09-17 20:53:38 +0430

    Add initial BizPay API endpoint

--- src/server.js
+++ src/server.js
@@ -3,6 +3,18 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "application/json" });
 
+  if (req.url === "/api") {
+    res.end(
+      JSON.stringify({
+        name: "BizPay",
+        version: "0.1.0",
+        status: "running",
+        message: "Welcome to BizPay API"
+      })
+    );
+    return;
+  }
+
   res.end(
     JSON.stringify({
       name: "BizPay",
